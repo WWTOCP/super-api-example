f0ba89785d1660b750cdf815b8ab7f07190873bc
bwu-contour <bing.wu@chase.com>
2024-10-31 14:31:55 -0700

    update hostname on the server

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+import platform
 
 app = FastAPI()
 
@@ -14,22 +15,30 @@ items = []
 
 @app.get("/")
 def root():
+    print("This request is being served by server: " + platform.node())
+
     return {"Hello": "World"}
 
 
 @app.post("/items")
 def create_item(item: Item):
+    print("This request is being served by server: " + platform.node())
+
     items.append(item)
     return items
 
 
 @app.get("/items", response_model=list[Item])
 def list_items(limit: int = 10):
+    print("This request is being served by server: " + platform.node())
+
     return items[0:limit]
 
 
 @app.get("/items/{item_id}", response_model=Item)
 def get_item(item_id: int) -> Item:
+    print("This request is being served by server: " + platform.node())
+
     if item_id < len(items):
         return items[item_id]
     else:
@@ -37,6 +46,8 @@ def get_item(item_id: int) -> Item:
         
 @app.delete("/items/{item_id}", response_model=Item)
 def delete_item(item_id: int) -> Item:
+    print("This request is being served by server: " + platform.node())
+
     if item_id < len(items):
         item = items.pop()
         return item
