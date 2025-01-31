96c9692a82ee32e2a22a39ac67e771a6bcd974ea
ATCadmin <bing.wu@chase.com>
2025-01-30 22:43:54 -0600

    change to Cilium

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import requests
 app = FastAPI()
 
 
-ENDPOINT_MARIADB = "https://qod-api-route-vpc-test-08-workload.apps.workload3.rh.wwtpoc.local"
-ENDPOINT_MYSQL = "https://qod-api-route-vpc-test-08-workload02.apps.workload3.rh.wwtpoc.local"
-ENDPOINT_COCKROACHDB = "https://qod-api-route-vpc-test-08-workload03.apps.workload3.rh.wwtpoc.local"
-ENDPOINT_MONGODB = "https://qod-api-route-vpc-test-08-workload04.apps.workload3.rh.wwtpoc.local"
-ENDPOINT_REDIS = "https://qod-api-route-vpc-test-08-workload05.apps.workload3.rh.wwtpoc.local"
-ENDPOINT_CASSANDRA = "https://qod-api-route-vpc-test-08-workload06.apps.workload3.rh.wwtpoc.local"
+ENDPOINT_MARIADB = "https://qod-api-route-vpc-test-08-workload.apps.w2rz2.rh.wwtpoc.local"
+ENDPOINT_MYSQL = "https://qod-api-route-vpc-test-08-workload02.apps.w2rz2.rh.wwtpoc.local"
+ENDPOINT_COCKROACHDB = "https://qod-api-route-vpc-test-08-workload03.apps.w2rz2.rh.wwtpoc.local"
+ENDPOINT_MONGODB = "https://qod-api-route-vpc-test-08-workload04.apps.w2rz2.rh.wwtpoc.local"
+ENDPOINT_REDIS = "https://qod-api-route-vpc-test-08-workload05.apps.w2rz2.rh.wwtpoc.local"
+ENDPOINT_CASSANDRA = "https://qod-api-route-vpc-test-08-workload06.apps.w2rz2.rh.wwtpoc.local"
 
 
 @app.get("/")
